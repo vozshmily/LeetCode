272d0286925d6e65c6389b8c04efcea23d862001
Buu <vozshmily@gmail.com>
2022-08-18 23:43:04 +0800

    leet 200

---
+++ 200_Number-of-Islands.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int NumIslands(char[][] grid) {
+        
+        int answer=0;
+            
+        for(int i=0;i<grid.Length;i++){
+            for(int j=0;j<grid[i].Length;j++){
+                if(grid[i][j]=='1'){
+                    answer+=1;
+                    DFS(grid,i,j);
+                }
+                
+            }
+        }
+        return answer;
+    }
+    public void DFS(char[][] grid,int i,int j){
+        if(i<0 || i>=grid.Length || j<0 || j>=grid[i].Length || grid[i][j]=='0')
+            return;
+        
+        grid[i][j]='0';
+        DFS(grid,i-1,j);
+        DFS(grid,i+1,j);
+        DFS(grid,i,j-1);
+        DFS(grid,i,j+1);
+    }
+}
